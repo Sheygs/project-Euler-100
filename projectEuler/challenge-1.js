ca60d862fa5f08c189c3d693fdc82e9b6c8fa91a
Olusegun <segun.ekoh@gmail.com>
2020-01-29 14:59:20 +0100

    minor changes

--- projectEuler/challenge-1.js
+++ projectEuler/challenge-1.js
@@ -5,20 +5,19 @@
   Find the sum of all the multiples of 3 or 5 below the provided parameter value "number".
 */
 
-function multiplesOf3and5(number){
-  if (typeof number !== 'number') return `argument must be an integer`;
+(function multiplesOf3or5(number){
+  if (!Number.isInteger(number)) return;
     let sum = 0;
-    for (let i = 0; i < number; i++){
+    for (let i = 3; i < number; i++){
       if ((i % 3) === 0 || (i % 5) === 0){
         sum+=i;
       }
     }
     return sum; 
-}
+})(1000); // 233168
 
-multiplesOf3and5(1000); // 233168
-multiplesOf3and5(49); // 543
-multiplesOf3and5(19564); // 89301183
+// multiplesOf3or5(49) => 543
+// multiplesOf3or5(19564) => 89301183
 
 
 // Code needs refactoring
