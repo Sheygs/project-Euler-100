9f90dfda0f49696c01f280908d4bbba6b613bc9a
Olusegun <segun.ekoh@gmail.com>
2020-01-29 13:16:55 +0100

    refactor code to IIFE

--- projectEuler/challenge-2.js
+++ projectEuler/challenge-2.js
@@ -1,5 +1,6 @@
-// Even Fibonacci Numbers
 /*
+ Even Fibonacci
+
  Each new term in the Fibonacci sequence is generated 
  by adding the previous two terms. By starting with 1 and 2,
  the first 10 terms will be:
@@ -10,7 +11,7 @@ By considering the terms in the Fibonacci sequence that do not
 exceed the nth term, find the sum of the even-valued terms.
 */
 
-function fiboEvenSum(n){
+(function fiboEvenSum(n){
   let f1 = 1,
       f2 = 2,
       sum = 2,
@@ -24,7 +25,8 @@ function fiboEvenSum(n){
       f2 = add;
   }
   return sum;
-}
+})(10); // 44
 
-fiboEvenSum(10); // 44
 fiboEvenSum(18); // 3382
+
+// Code needs refactoring
