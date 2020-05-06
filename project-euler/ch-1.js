131b4bb978b251d9dcdcf90059ed829de52683c8
sheygs13 <segun.ekoh@gmail.com>
2020-05-06 10:46:53 +0100

    refactor challenge 1 :rocket:

--- project-euler/ch-1.js
+++ project-euler/ch-1.js
@@ -1,25 +1,29 @@
-// Find the sum of all the multiples of 3 or 5 below the provided parameter value
-// refactor code
+// Find the sum of all the multiples of 3 or 5 
+// below the provided parameter value
 
-(function multiplesOf3or5(number){
-  if (!Number.isInteger(number)) return;
-    let sum = 0;
-    for (let i = 3; i < number; i++){
-      if ((i % 3) === 0 || (i % 5) === 0){
-        sum+=i;
-      }
-    }
-    return sum; 
-})(1000); // 233168
-
-
-function isMultiple(num){
-  if (!Number.isInteger(num)) return;
-  if (num % 3 === 0 || num % 5 === 0) return true;
-  return num;
+// finds the sequence of a value 
+// below a provided parameter value
+// (finalValue - 1) stops at the value one less
+// of the given parameter value
+function range(initialValue, finalValue) {
+  return Array.from({ length: ((finalValue - 1) - initialValue) + 1}, (_,index) => initialValue + index);
 }
 
-function range(num){}
+// Is multiple of 3 or 5 ?
+function isMultiple(num){
+  if (!Number.isInteger(num)) return;
+  if (!(num % 3) || !(num % 5)) return true;
+  return false;
+}
 
-function sumAll(num){}
+// filters only truthy value 
+function multiplesOnly(array){
+  return array.filter(el => isMultiple(el));
+}
+
+function sumAll(array){
+ return array.reduce((acc,next) => acc + next,0);
+}
+
+const sum = sumAll(multiplesOnly(range(0,1000))); 
 
